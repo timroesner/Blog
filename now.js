8b22f66e50ec7b4af5f0464a9b2064819e6b39b2
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2019-07-06 21:13:22 -0700

    Add alias

--- now.js
+++ now.js
@@ -1,6 +1,7 @@
 {
   "version": 2,
-  "name": "my-jekyll-project",
+  "name": "my-blog",
+  "alias": ["blog.timroesner.com", "www.blog.timroesner.com"],
   "builds": [
     { "src": "build.sh", "use": "@now/static-build", "config": { "distDir": "_site" } }
   ]
